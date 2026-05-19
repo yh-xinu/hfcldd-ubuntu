e33d047bb3286a34423155c416a1fcadf8186732
YH Kim <yh90.kim@gmail.com>
2026-05-19 08:23:42 +0000

    5th patch apply for 3 files

--- hfcldd_make/hfcl_handler_trace_fx.h
+++
@@ -1,9 +0,0 @@
-/*
- * hfcl_handler_trace_fx.h 
- * Copyright (C) 2007, 2015, Hitachi, Ltd.
- * Author(s): Yoshihiro Toyohara <yoshihiro.toyohara.qs@hitachi.com>
- */
-/*
- * $Id: hfcl_handler_trace_fx.h,v 1.1.2.2.2.1.2.1 2015/02/04 08:32:48 toyo Exp $
- */
-
